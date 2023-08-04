c685a0ee3f65315cf00b67f88f0205691b968f07
swapnilhampi0201 <141388689+swapnilhampi0201@users.noreply.github.com>
2023-08-04 01:20:29 -0400

    Update strimlit_app.py

--- strimlit_app.py
+++ strimlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My parents new health dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Fruits')
+streamlit.title('Cornflakes')
+streamlit.title('Tea')
+
