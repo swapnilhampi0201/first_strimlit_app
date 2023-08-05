9ab2c82e4571d6b38cb62ecb278a501facc66346
swapnilhampi0201 <141388689+swapnilhampi0201@users.noreply.github.com>
2023-08-05 15:33:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ if streamlit.button('get fruit load list'):
       my_data_rows = get_fruit_load_list()
       streamlit.dataframe(my_data_rows)
 
-streamlit.text(my_data_row)
+streamlit.text(my_data_rows)
 
 
 
