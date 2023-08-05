34ff42ba2e9e76f94bb03a210e32932f3d833bf6
swapnilhampi0201 <141388689+swapnilhampi0201@users.noreply.github.com>
2023-08-05 13:37:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,7 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
+
+my_data_row=my_cur.fetchcone()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
